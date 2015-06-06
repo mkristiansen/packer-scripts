92b3fa0e540a334d316bd4644e959a5b331d6052
Morten Kristiansen <morten.kristainsen@gmail.com>
2015-06-06 18:13:40 +0100

    refactored paths, removed linefeed from awk statement

--- updatePrecise64Chef.sh
+++ updatePrecise64Chef.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-cd bento/packer
-packer build -only=parallels-iso ubuntu-12.04-amd64-chef-latest.json -machine-readable > packer.out
-BOX=$(awk '/provider box/ { print $6}' packer.out)
-vagrant box remove parallels/ubuntu-12.04
-vagrant box add $BOX --name "parallels/ubuntu-12.04"
+cd ../bento/packer
+packer build -only=parallels-iso ubuntu-12.04-amd64-chef-latest.json -machine-readable > ../../packer.out
+BOX=$(awk '/provider box/ { printf $6}' ../../packer.out)
+vagrant box remove my/parallels/ubuntu-12.04
+vagrant box add $BOX --name "my/parallels/ubuntu-12.04"
 rm $BOX
