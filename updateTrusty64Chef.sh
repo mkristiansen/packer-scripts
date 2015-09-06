031be9215591039f9f1e60675ed32fecf3b93449
Morten Kristiansen <morten.kristainsen@gmail.com>
2015-09-06 12:31:22 +0100

    Updated ubuntu scripts after merge of chef/bento into master

--- updateTrusty64Chef.sh
+++ updateTrusty64Chef.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-cd ../bento/packer
-packer build -only=parallels-iso ubuntu-14.04-amd64-chef-latest.json -machine-readable > ../../packer.out
-BOX=$(awk '/provider box/ { print $4;exit}' ../../packer.out)
+cd ../bento
+packer build -only=parallels-iso ubuntu-14.04-amd64.json -machine-readable > ../packer.out
+BOX=$(awk '/provider box/ { print $4;exit}' ../packer.out)
 vagrant box remove my/parallels/ubuntu-14.04
 vagrant box add $BOX --name "my/parallels/ubuntu-14.04"
 rm $BOX
--- updateVivid64Chef.sh
+++ updateVivid64Chef.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-cd ../bento/packer
-packer build -only=parallels-iso ubuntu-15.04-amd64-chef-latest.json -machine-readable > ../../packer.out
-BOX=$(awk '/provider box/ { print $4;exit}' ../../packer.out)
+cd ../bento
+packer build -only=parallels-iso ubuntu-15.04-amd64.json -machine-readable > ../packer.out
+BOX=$(awk '/provider box/ { print $4;exit}' ../packer.out)
 vagrant box remove my/parallels/ubuntu-15.04
 vagrant box add $BOX --name "my/parallels/ubuntu-15.04"
 rm $BOX
