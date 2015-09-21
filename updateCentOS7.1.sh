bce715d7e29533326d437f7c4dd3284e45619e38
Morten Kristiansen <morten.kristainsen@gmail.com>
2015-09-21 16:34:22 +0100

    mixup box name to remove

--- updateCentOS7.1.sh
+++ updateCentOS7.1.sh
@@ -3,6 +3,6 @@
 cd ../bento
 packer build -only=parallels-iso centos-7.1-x86_64.json -machine-readable > ../packer.out
 BOX=$(awk '/provider box/ { print $4;exit}' ../packer.out)
-vagrant box remove parallels/centos-7.0
+vagrant box remove parallels/centos-7.1
 vagrant box add $BOX --name "my/parallels/centos-7.1"
 rm $BOX
